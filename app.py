cab5738dbf5d74108456a25d554c431d43b1efa7
constantineandcon-ux <constantineandcon@gmail.com>
2026-02-15 04:26:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
-app = Flask(__name__)
+app = Flask(name)
 
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+if not os.path.exists(UPLOAD_FOLDER):
+os.makedirs(UPLOAD_FOLDER)
 
 @app.route("/")
-def home():
-    return "Η εφαρμογή ANTKON λειτουργεί!"
+def index():
+return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route("/upload", methods=["POST"])
+def upload():
+file = request.files["file"]
+if file.filename != "":
+filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+file.save(filepath)
+return redirect(url_for("index"))
 
+if name == "main":
+app.run()
