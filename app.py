4c5c6fcaf9123cc07fe8a29defb57fff52631b1b
constantineandcon-ux <constantineandcon@gmail.com>
2026-02-15 08:10:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, send_file
-from flask_cors import CORS
+from flask import Flask, render_template, request, redirect
 import psycopg2
-from io import BytesIO
+import os
 
 app = Flask(__name__)
-CORS(app)
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
@@ -18,75 +15,48 @@ def index():
     cur = conn.cursor()
     cur.execute("SELECT id, amka, first_name, last_name FROM patients ORDER BY created_at DESC")
     patients = cur.fetchall()
-    cur.close()
     conn.close()
     return render_template("index.html", patients=patients)
 
 @app.route("/add_patient", methods=["POST"])
 def add_patient():
+    amka = request.form["amka"]
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    birth_date = request.form["birth_date"]
+    phone = request.form["phone"]
+    email = request.form["email"]
+
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
         INSERT INTO patients (amka, first_name, last_name, birth_date, phone, email)
         VALUES (%s,%s,%s,%s,%s,%s)
-    """, (
-        request.form["amka"],
-        request.form["first_name"],
-        request.form["last_name"],
-        request.form["birth_date"],
-        request.form["phone"],
-        request.form["email"]
-    ))
+    """,(amka,first_name,last_name,birth_date,phone,email))
     conn.commit()
-    cur.close()
     conn.close()
-    return redirect(url_for("index"))
+    return redirect("/")
 
-@app.route("/upload/<int:patient_id>", methods=["POST"])
+@app.route("/upload_file/<int:patient_id>", methods=["POST"])
 def upload_file(patient_id):
     file = request.files["file"]
+    file_data = file.read()
+
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
         INSERT INTO medical_files (patient_id, file_name, file_type, file_data)
         VALUES (%s,%s,%s,%s)
-    """, (
-        patient_id,
-        file.filename,
-        file.content_type,
-        psycopg2.Binary(file.read())
-    ))
+    """,(patient_id,file.filename,file.content_type,file_data))
     conn.commit()
-    cur.close()
     conn.close()
-    return redirect(url_for("index"))
+    return redirect("/")
 
-@app.route("/file/<int:file_id>")
-def get_file(file_id):
+@app.route("/view_file/<int:file_id>")
+def view_file(file_id):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("SELECT file_name, file_type, file_data FROM medical_files WHERE id=%s", (file_id,))
+    cur.execute("SELECT file_name, file_type, file_data FROM medical_files WHERE id=%s",(file_id,))
     file = cur.fetchone()
-    cur.close()
     conn.close()
-    return send_file(BytesIO(file[2]), download_name=file[0], mimetype=file[1])
-
-@app.route("/search", methods=["POST"])
-def search():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT id, amka, first_name, last_name
-        FROM patients
-        WHERE amka=%s OR last_name ILIKE %s
-    """, (
-        request.form["search"],
-        f"%{request.form['search']}%"
-    ))
-    patients = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template("index.html", patients=patients)
-
-if __name__ == "__main__":
-    app.run()
+    return (file[2], 200, {"Content-Type": file[1]})
