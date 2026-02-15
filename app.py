b9220ca34075119e97767dbf88299d7cca730ff8
constantineandcon-ux <constantineandcon@gmail.com>
2026-02-15 03:43:38 +0200

    Update app.py
    Διόρθωση name

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def home():
-    return "<h1>Ιατρικό Σύστημα Συνταγογράφησης</h1>"
+    return "Η εφαρμογή ANTKON λειτουργεί!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
+
